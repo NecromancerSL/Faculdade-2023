407c8139b474707211bbb0048bcdb98209c116b5
Gustavo Nunes <dsnunes.g42@outlook.com>
2023-02-22 20:44:08 -0300

    Iniciando o repositório da faculdade 2023

---
+++ Desenvolvimento de Software 2 (Node)/express/app.js
@@ -0,0 +1,23 @@
+var express = require('express'); //utilizar o express
+var  app = express(); //instancia o express
+
+app.use(express.static('public')); //permite acesso as paginas
+
+var path = require('path');
+
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname + '/public/page/index.html');
+})
+
+app.get('/sobre', (req,res) =>{
+    res.sendFile(__dirname + '/public/page/sobre.html');
+})
+
+app.get('/contato', (req,res) =>{
+    res.sendFile(__dirname + '/public/page/contato.html');
+})
+
+
+app.listen(8081,() =>{
+    console.log("escutando")
+})//fazer o servidor rodar
