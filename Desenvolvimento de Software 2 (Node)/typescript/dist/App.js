345f736cd5e93c34724e01cd53a5110d8f83f9ad
Gustavo Nunes <dsnunes.g42@outlook.com>
2023-03-01 17:26:59 -0300

    aula de sexta 24/02

---
+++ Desenvolvimento de Software 2 (Node)/typescript/dist/App.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = 'alexandre';
+let idade = 30;
+console.log('Meu nome é ', nome, "e eu tenho ", idade, "anos");
---
+++ Desenvolvimento de Software 2 (Node)/typescript/src/App.ts
@@ -0,0 +1,4 @@
+let nome: string = 'alexandre';
+let idade: number = 30;
+
+console.log('Meu nome é ',nome, "e eu tenho ",idade, "anos");
